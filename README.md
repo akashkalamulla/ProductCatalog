f614acf8fe9aca3cee8002cc0ddcfc628a9e680e
Isuru Yahampath <isuruyahampath1@gmail.com>
2023-10-26 06:47:55 +0530

    somewhat major db changes

--- README.md
+++ README.md
@@ -10,29 +10,28 @@ Product catalog microservice for an e-commerce application
 create database productcatalog;
 use productcatalog;
 
-CREATE TABLE catalog(
-id INT AUTO_INCREMENT,
-productid INT,
-productname VARCHAR(255) NOT NULL,
-Productdescription VARCHAR(255) NOT NULL,
-category VARCHAR(255) NOT NULL,
-brand VARCHAR(255) NOT NULL,
-price double NOT NULL,
-currency VARCHAR(255) NOT NULL,
-weight INT NOT NULL,
-dimension VARCHAR(255) NOT NULL,
-stockquantity INT NOT NULL,
-supplier VARCHAR(255) NOT NULL,
-imgurls varchar(255) NOT NULL,
-tags VARCHAR(255) NOT NULL,
-reviewrate float NOT NULL,
-numreview INT NOT NULL,
-PRIMARY KEY (id)
+CREATE TABLE catalog
+(
+    id                 INT AUTO_INCREMENT,
+    productname        VARCHAR(50) NOT NULL,
+    Productdescription VARCHAR(255) NOT NULL,
+    category           VARCHAR(10) NOT NULL,
+    brand              VARCHAR(30) NOT NULL,
+    price              double       NOT NULL,
+    currency           VARCHAR(3) NOT NULL,
+    weight             FLOAT          NOT NULL,
+    dimension          VARCHAR(20) NOT NULL,
+    stockquantity      INT          NOT NULL,
+    imgurl            varchar(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO catalog (productid ,productname, Productdescription, category, brand, price, currency, weight, dimension, stockquantity, supplier, imgurls, tags, reviewrate, numreview)
-VALUES
-(232506,'samsundj4', 'Description for Product 1', 'medium budjet', 'smasung', 299.99, 'USD', 20, '10x5x3', 100, 'samsundhjx', 'image_url_1',  'Tag 1, Tag 2', 4.5, 50),
-(232507,'iphone14', 'Description for Product 2', 'high budjet', 'apple', 49.99, 'USD', 1, '14x10x2', 200, 'porhxy', 'image_url_2', 'Tag 2, Tag 3', 4.0, 30),
-(232508,'nokia36', 'Description for Product 3', 'low budjet', 'nokia', 79.99, 'USD', 3, '8x8x8', 75, 'dearyoy', 'image_url_3','Tag 4, Tag 5', 4.2, 65);
+INSERT INTO catalog (productname, Productdescription, category, brand, price, currency, weight, dimension,
+                     stockquantity, imgurl)
+VALUES (232506, 'samsundj4', 'Description for Product 1', 'medium budjet', 'smasung', 299.99, 'USD', 20, '10x5x3', 100,
+        'samsundhjx', 'image_url_1', 'Tag 1, Tag 2', 4.5, 50),
+       (232507, 'iphone14', 'Description for Product 2', 'high budjet', 'apple', 49.99, 'USD', 1, '14x10x2', 200,
+        'porhxy', 'image_url_2', 'Tag 2, Tag 3', 4.0, 30),
+       (232508, 'nokia36', 'Description for Product 3', 'low budjet', 'nokia', 79.99, 'USD', 3, '8x8x8', 75, 'dearyoy',
+        'image_url_3', 'Tag 4, Tag 5', 4.2, 65);
 ```
--- src/main/java/com/Product/Catalog/model/Catalog.java
+++ src/main/java/com/Product/Catalog/model/Catalog.java
@@ -1,169 +1,43 @@
 package com.Product.Catalog.model;
 
 import javax.persistence.*;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "catalog")
-public class Catalog{
+public class Catalog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "productid")
-    private int ProductId;
+
     @Column(name = "productname")
     private String ProductName;
+
     @Column(name = "Productdescription")
     private String Description;
+
     @Column(name = "category")
     private String Category;
+
     @Column(name = "brand")
     private String Brand;
+
     @Column(name = "price")
     private double Price;
+
     @Column(name = "currency")
     private String Currency;
+
     @Column(name = "weight")
-    private int Weight;
+    private float Weight;
+
     @Column(name = "dimension")
     private String Dimensions;
+
     @Column(name = "stockquantity")
     private int Stock_Quantity;
-    @Column(name = "supplier")
-    private String Supplier;
-    @Column(name = "imgurls")
-    private String ImgUrls;
+    @Column(name = "imgurl")
+    private String ImgUrl;
 
-    @Column(name = "tags")
-    private String Tags;
-    @Column(name = "reviewrate")
-    private float ReviewRate;
-    @Column(name = "numreview")
-    private int NumReviews;
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getProductId() {
-        return ProductId;
-    }
-
-    public void setProductId(int productId) {
-        ProductId = productId;
-    }
-
-    public String getProductName() {
-        return ProductName;
-    }
-
-    public void setProductName(String productName) {
-        ProductName = productName;
-    }
-
-    public String getDescription() {
-        return Description;
-    }
-
-    public void setDescription(String description) {
-        Description = description;
-    }
-
-    public String getCategory() {
-        return Category;
-    }
-
-    public void setCategory(String category) {
-        Category = category;
-    }
-
-    public String getBrand() {
-        return Brand;
-    }
-
-    public void setBrand(String brand) {
-        Brand = brand;
-    }
-
-    public double getPrice() {
-        return Price;
-    }
-
-    public void setPrice(double price) {
-        Price = price;
-    }
-
-    public String getCurrency() {
-        return Currency;
-    }
-
-    public void setCurrency(String currency) {
-        Currency = currency;
-    }
-
-    public int getWeight() {
-        return Weight;
-    }
-
-    public void setWeight(int weight) {
-        Weight = weight;
-    }
-
-    public String getDimensions() {
-        return Dimensions;
-    }
-
-    public void setDimensions(String dimensions) {
-        Dimensions = dimensions;
-    }
-
-    public int getStock_Quantity() {
-        return Stock_Quantity;
-    }
-
-    public void setStock_Quantity(int stock_Quantity) {
-        Stock_Quantity = stock_Quantity;
-    }
-
-    public String getSupplier() {
-        return Supplier;
-    }
-
-    public void setSupplier(String supplier) {
-        Supplier = supplier;
-    }
-
-    public String getImgUrls() {
-        return ImgUrls;
-    }
-
-    public void setImgUrls(String imgUrls) {
-        ImgUrls = imgUrls;
-    }
-
-    public String getTags() {
-        return Tags;
-    }
-
-    public void setTags(String tags) {
-        Tags = tags;
-    }
-
-    public float getReviewRate() {
-        return ReviewRate;
-    }
-
-    public void setReviewRate(float reviewRate) {
-        ReviewRate = reviewRate;
-    }
-
-    public int getNumReviews() {
-        return NumReviews;
-    }
-
-    public void setNumReviews(int numReviews) {
-        NumReviews = numReviews;
-    }
 }
